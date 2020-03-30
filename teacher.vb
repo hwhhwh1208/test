b433132a2c9dac7eb5ceca3904e117544c1538fd
hwhhwh1208 <1968281857@qq.com>
2020-03-30 17:11:53 +0800

    作了一些处理

---
+++ teacher.vb
@@ -0,0 +1,14 @@
+Imports System
+
+Public Class Teacher
+
+    Public Sub Main(args As String())
+
+
+
+    End Sub
+
+    Public Sub SayHello()
+        Console.WriteLine("hello world")
+    End Sub
+End Class
