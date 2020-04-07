caed913030e6a82861544cad09f5de4f96aa59a2
侯文辉 <houwh@digiwin.com>
2020-04-07 10:39:06 +0800

    追加了一条输出信息

--- Study_1.vb
+++ Study_1.vb
@@ -2,8 +2,9 @@
 Public Class Study_1
 	Sub PrintHelloWorld()
 
-		Console.WriteLine("Hello World")
-		Console.ReadKey()
+        Console.WriteLine("Hello World")
+        Console.WriteLine("Hello GitHub")
+        Console.ReadKey()
 	End Sub
 
 End Class
